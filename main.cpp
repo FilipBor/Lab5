7d638300671ea554588c94790d80484f94f57fc8
FilipBor <filibor847@student.polsl.pl>
2020-11-05 13:58:38 +0100

    Experiment w bidirectional graph

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ struct Node
 {
     int value;
     std::vector<Edge> edges;
-
+    bool visited = false;
     Node(int v)
     {
         this->value = v;
@@ -28,12 +28,26 @@ struct Node
         Edge e(w, next);
         edges.push_back(e);
     }
+
 };
 
+void dfs (Node* node)
+{
+    std::cout<< node->value <<std::endl;
+    node->visited = true;
+    for ( int i = 0; i < node->edges.size(); i++)
+    {
+        if (!node->edges[i].next->visited)
+        {
+            dfs(node->edges[i].next);
+        }
+    }
+
+}
 int main(){
     std::vector<Node> graph;
 
-    Node n0(0);
+    Node n0(0); /** ROOT */
     Node n1(1);
     Node n2(2);
     Node n3(3);
@@ -58,6 +72,13 @@ int main(){
     graph.push_back(n5);
     graph.push_back(n6);
 
+    n3.connect(0, &n1);
+    n4.connect(0, &n1);
 
+    n5.connect(0, &n2);
+    n6.connect(0, &n3);
+    dfs(&n0);
+
+    return 0;
 }
 
